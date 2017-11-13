8bd965bea2cc63f54cfc785a39aa4dfdda876c18
t303710 <nidhin.vrindavan@klm.com>
2017-11-14 00:10:22 +0530

    for verifying fb request

--- socialbids-web/src/main/groovy/com/hackathon/socialbids/controller/SocialBids.groovy
+++ socialbids-web/src/main/groovy/com/hackathon/socialbids/controller/SocialBids.groovy
@@ -3,18 +3,34 @@ package com.hackathon.socialbids.controller
 import groovy.util.logging.Slf4j
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 import static org.springframework.http.HttpStatus.OK
+import static org.springframework.http.HttpStatus.UNAUTHORIZED
+import static org.springframework.web.bind.annotation.RequestMethod.GET
 
 @RestController
-@RequestMapping('/socialbids/healthcheck')
+@RequestMapping('/socialbids')
 @Slf4j
 class SocialBids {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = 'healthcheck', method = GET)
     ResponseEntity webhook() {
         log.info('App has started!!!')
         new ResponseEntity(OK)
+    }
+
+    @RequestMapping(value= 'verify', method = GET)
+    ResponseEntity<String> verifyChallenge(@RequestParam('hub.mode') String mode,
+                                           @RequestParam('hub.challenge') String challenge,
+                                           @RequestParam('hub.verify_token') String verifyToken) {
+        if ('subscribe' == mode && 'socialbidsverify' == verifyToken) {
+            log.info('Webhook registration successful')
+            ResponseEntity.ok(challenge)
+        } else {
+            log.error('Webhook registration failed for token : {}', verifyToken)
+            new ResponseEntity('Webhook registration failed for token', UNAUTHORIZED)
+        }
+
     }
 }
