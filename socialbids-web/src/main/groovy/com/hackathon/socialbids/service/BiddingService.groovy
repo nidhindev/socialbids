89edaefb07d69d460681a6e88363dfd8f716c380
t234722 <vinod.penta@klm.com>
2017-11-15 18:07:11 +0530

    notifying customer if he is over bided

--- socialbids-web/src/main/groovy/com/hackathon/socialbids/service/BiddingService.groovy
+++ socialbids-web/src/main/groovy/com/hackathon/socialbids/service/BiddingService.groovy
@@ -19,7 +19,7 @@ class BiddingService {
     @Autowired
     CustomerRepository customerRepository;
 
-    Map<String, String> customersIdNameMap = ['1563047500438143':'Vinod','1486773568044876':'Divya', '1769834176420354':'Nidhin']
+    Map<String, String> customersIdNameMap = ['1563047500438143': 'Vinod', '1486773568044876': 'Divya', '1769834176420354': 'Nidhin']
 
     void sendBidValueToCustomer(String id) {
         def flights = flightRepository.findAll()
@@ -30,8 +30,6 @@ class BiddingService {
             customersPlacedBid.forEach() {
                 if (it.amount > minBidAmount) {
                     minBidAmount = it.amount
-                   // def message = new BidMessage(id: id, type: 'quickReply', replyOptions: ['+10','+15','+20','No'], message: 'you got over bid by another passenger. Do you want to raise your bid')
-                    //customerService.sendToCustomer(message, 'quickReply')
                 }
             }
         }
@@ -39,22 +37,26 @@ class BiddingService {
         customerService.sendToCustomer(message, 'quickReply')
     }
 
-    void saveCustomerBid(String id, Long amountIncrement){
+    void saveCustomerBid(String id, Long amountIncrement) {
 
         def flights = flightRepository.findAll()
         def flight = flights[0]
         def minBidAmount = flight.upperBidValue
         def customersPlacedBid = customerRepository.findAll()
+        def oldCustomer;
         if (customersPlacedBid.size() > 0) {
             customersPlacedBid.forEach() {
                 if (it.amount > minBidAmount) {
                     minBidAmount = it.amount
+                    oldCustomer = it
                 }
             }
         }
-        def customer = new Customer(customerId: id, customerName: customersIdNameMap.get(id), amount: minBidAmount+amountIncrement)
+        def customer = new Customer(customerId: id, customerName: customersIdNameMap.get(id), amount: minBidAmount + amountIncrement)
         customerRepository.save(customer)
         def message = new BidMessage(id: id, type: 'text', message: '1F5FF')
         customerService.sendToCustomer(message, 'text')
+        def message2 = new BidMessage(id: oldCustomer.id, type: 'quickReply', replyOptions: ['+10', '+15', '+20', 'No'], message: 'you got over bid by another passenger with ' + minBidAmount + amountIncrement + '. Do you want to raise your bid')
+        customerService.sendToCustomer(message, 'quickReply')
     }
 }
